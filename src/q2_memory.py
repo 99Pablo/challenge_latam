68c0fc1c39e0ba79edd0092daa404f98ff4d0292
Pablo Sepulveda <pablosepulvedanavarrete@gmail.com>
2023-10-04 12:34:39 -0300

    challenge

---
+++ src/q2_memory.py
@@ -0,0 +1,25 @@
+import emoji
+import json
+
+def q2_memory(path_file: str):
+    result = []
+
+    emojis = {}
+
+    with open(path_file) as f:
+        for line in f:
+            tweet = json.loads(line)
+            
+            content = tweet['content']
+            content_emojis = emoji.emoji_list(content)
+            
+            for emo in content_emojis:
+                try:
+                    emojis[emo['emoji']] += 1
+                except KeyError:
+                    emojis[emo['emoji']] = 1
+
+    for emo, count in sorted(emojis.items(), key=lambda emo: emo[1], reverse=True)[:10]:
+        result.append((emo, count))
+
+    return result
